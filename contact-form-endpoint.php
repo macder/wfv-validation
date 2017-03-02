6e02fe763cf1e8deec3b1863b009e06d351eaadb
Maciej Derulski <maciej@derulski.com>
2017-03-02 03:26:29 -0500

    base class structure

--- contact-form-endpoint.php
+++ contact-form-endpoint.php
@@ -9,3 +9,11 @@ Author URI:  https://derulski.com
 License:     GPL3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 */
+
+class Contact_Form_Endpoint {
+  function __construct() {
+    
+  }
+}
+ 
+$Contact_Form_Endpoint = new Contact_Form_Endpoint();
