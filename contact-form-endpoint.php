c87d41a2193c6a51ea089dac57859c86c0ff94ba
Maciej Derulski <maciej@derulski.com>
2017-03-03 23:48:39 -0500

    added action hooks for form post, and docs

--- contact-form-endpoint.php
+++ contact-form-endpoint.php
@@ -1,9 +1,9 @@
-<?php defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+<?php defined( 'ABSPATH' ) or die();
 /*
 Plugin Name: Contact Form Endpoint
 Plugin URI:  https://github.com/macder/wp-contact-form-endpoint
 Description: Simple api endpoint to post data from a contact form. Intended for developers
-Version:     0.1
+Version:     0.0.1
 Author:      Maciej Derulski
 Author URI:  https://derulski.com
 License:     GPL3
@@ -11,9 +11,34 @@ License URI: https://www.gnu.org/licenses/gpl-3.0.html
 */
 
 class Contact_Form_Endpoint {
+
   function __construct() {
-    
+    $this->add_actions();
   }
+
+  /**
+   * creates the action hooks for contact form post
+   *
+   * @since 0.0.1
+   * @access private
+   */
+  private function add_actions() {
+    add_action( 'admin_post_nopriv_contact_form', array( $this, 'post_entry' ) );
+    add_action( 'admin_post_contact_form', array( $this, 'post_entry' ) );
+  }
+
+  /**
+   * callback for contact_form post action
+   *
+   * prepares $_POST data for sanitation and validation
+   *
+   * @since 0.0.1
+   * @access private
+   */
+  private function post_entry() {
+
+  }
+
 }
  
 $Contact_Form_Endpoint = new Contact_Form_Endpoint();
