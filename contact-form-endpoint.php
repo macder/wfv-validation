202a1e39b2411098e48292cf764a71847cce31f6
Maciej Derulski <maciej@derulski.com>
2017-03-04 01:22:47 -0500

    phpdocs for class, members, and methods

--- contact-form-endpoint.php
+++ contact-form-endpoint.php
@@ -12,32 +12,58 @@ License URI: https://www.gnu.org/licenses/gpl-3.0.html
 
 require "vendor/wixel/gump/gump.class.php";
 
+/**
+ * Summary.
+ *
+ * Description.
+ *
+ * @since 0.0.1
+ */
 class Contact_Form_Endpoint {
 
+	/**
+	 * Summary.
+	 *
+	 * @since 0.0.1
+	 * @access protected
+	 * @var array $validation_rules Form validation rules.
+	 */
+	protected $validation_rules;
+
+	/**
+	 * Class constructor
+	 *
+	 * @since 0.0.1
+	 *
+	 */
 	function __construct() {
 		$this->add_actions();
 	}
 
 	/**
-	* creates the action hooks for contact form post
-	*
-	* @since 0.0.1
-	* @access private
-	*/
+	 * creates the action hooks for contact form post
+	 *
+	 * @since 0.0.1
+	 * @access private
+	 */
 	private function add_actions() {
 		add_action( 'admin_post_nopriv_contact_form', array( $this, 'post_entry' ) );
 		add_action( 'admin_post_contact_form', array( $this, 'post_entry' ) );
 	}
 
-	/**
-	* callback for contact_form post action
-	*
-	* prepares $_POST data for sanitation and validation
-	*
-	* @since 0.0.1
-	*/
-	public function post_entry() {
+	private function validation_rules() {
 
+	}
+
+	/**
+	 * callback for contact_form post action
+	 *
+	 * prepares $_POST data for sanitation and validation
+	 *
+	 * @since 0.0.1
+	 */
+	public function post_entry() {
+		// where the magic starts
 	}
 
 }
