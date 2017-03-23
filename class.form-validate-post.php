41b4022b56ef043bb314028f787329b058b916ce
Maciej Derulski <macder7@gmail.com>
2017-03-22 23:47:16 -0400

    new class to handle the POST

---
+++ class.form-validate-post.php
@@ -0,0 +1,15 @@
+<?php defined( 'ABSPATH' ) or die();
+
+/**
+ * Summary
+ *
+ * Description
+ *
+ * @since 0.2.0
+ */
+class Form_Validate_Post {
+
+  function __construct() {
+    echo 'test';
+  }
+}
