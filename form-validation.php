f8d00e82c6aca8b32c8acb77b9482f54e097ee15
Maciej Derulski <macder7@gmail.com>
2017-03-23 02:38:37 -0400

    constant for validation pass action name

--- form-validation.php
+++ form-validation.php
@@ -13,7 +13,9 @@ License URI: https://www.gnu.org/licenses/gpl-3.0.html
 define( 'FORM_VALIDATION_VERSION', '0.2' );
 define( 'FORM_VALIDATION__MINIMUM_WP_VERSION', '4.7' ); // not tested with other versions
 define( 'FORM_VALIDATION__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
 define( 'FORM_VALIDATION__ACTION_POST', 'validate_form' );
+define( 'FORM_VALIDATION__PASS', 'valid_' );
 
 require_once( FORM_VALIDATION__PLUGIN_DIR . '/vendor/vlucas/valitron/src/Valitron/Validator.php' );
 require_once( FORM_VALIDATION__PLUGIN_DIR . 'class.form-validation.php' );
