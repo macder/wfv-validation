b427d9240f0826bdb88970daf4611a2517291175
Maciej Derulski <macder7@gmail.com>
2017-03-23 01:08:40 -0400

    callback to do the validation

--- form-validation.php
+++ form-validation.php
@@ -13,7 +13,15 @@ License URI: https://www.gnu.org/licenses/gpl-3.0.html
 define( 'FORM_VALIDATION_VERSION', '0.2' );
 define( 'FORM_VALIDATION__MINIMUM_WP_VERSION', '4.7' ); // not tested with other versions
 define( 'FORM_VALIDATION__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'FORM_VALIDATION__ACTION_POST', 'validate_form' );
 
 require_once( FORM_VALIDATION__PLUGIN_DIR . '/vendor/vlucas/valitron/src/Valitron/Validator.php' );
 require_once( FORM_VALIDATION__PLUGIN_DIR . 'class.form-validation.php' );
 require_once( FORM_VALIDATION__PLUGIN_DIR . 'class.form-validate-post.php' );
+
+add_action(FORM_VALIDATION__ACTION_POST, 'validate', 10, 1);
+function validate($rules) {
+    $result = new Form_Validate_Post($rules);
+    // test();
+    // print_r($v);
+}
