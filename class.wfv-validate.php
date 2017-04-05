6b3b3c43065708307462ed39b5a4fe0b90884bcc
Maciej Derulski <macder7@gmail.com>
2017-04-05 02:47:34 -0400

    validate method refactor for public access

--- class.wfv-validate.php
+++ class.wfv-validate.php
@@ -57,6 +57,7 @@ class WFV_Validate {
   public $nonce_field;
 
 
+
   /**
    * __construct
    *
@@ -68,21 +69,26 @@ class WFV_Validate {
 
   }
 
-    // $this->lorem = 'w0w';
+  /**
+   * Do the validation
+   *
+   * @since 0.2.0
+   * @since 0.6.0 Public access
+   */
+  public function validate() {
+    $this->validate_nonce();
+    $v = $this->create_valitron();
 
-    // print_r($this);
-    // $this->create_valitron( /*$form*/ );
-    /*$this->validate_nonce( $form->action );
-    $this->sanitize_post();
-    $this->create_valitron( $form );
-    $this->validate( $form );*/
-    // print_r($this);
+    if ( $v->validate() ) {
+      do_action( $this->action, $this->input );
+    } else {
+      $this->errors = $v->errors();
+    }
   }
 
   /**
    * Verify the nonce
    * Prevents CSFR exploits
-   *
    *
    * @since 0.2.2
    * @param string $action
@@ -99,85 +105,55 @@ class WFV_Validate {
    * Sanitize input and keys in $_POST
    * Assign the sanitized data to $sane_post property
    *
-   *
    * @since 0.2.0
-   * @access private
+   * @since 0.6.0 Public access
    */
-  private function sanitize_post() {
+  public function sanitize_post() {
     foreach ( $_POST as $key => $value ) {
       $this->input[ sanitize_key( $key ) ] = sanitize_text_field( $value );
     }
   }
 
-  /**
-   * Do the validation
-   *
-   *
-   * @since 0.2.0
-   * @since 0.6.0 Public access
-   */
-  public function validate() {
-    // echo $this->action;
-    $this->validate_nonce();
-    // $v = $this->valitron;
-
-    // if ( $v->validate() ) {
-    //   do_action( $form->action, $this->input );
-    // } else {
-    //   $this->validate_fail();
-    // }
-  }
-
-
-  /**
-   * Validation failed
-   *
-   *
-   * @since 0.2.0
-   * @access private
-   */
-  private function validate_fail() {
-    $url = add_query_arg( $this->input, wp_get_referer() );
-    wp_safe_redirect( $url );
-  }
 
   /**
    * Create an instance of Valitron\Validator with our rules / messages
    * Assign to $valitron property
    *
-   *
    * @since 0.2.0
    * @param array $form Form configuration array
    */
-  public function create_valitron( $form ) {
-    $this->valitron = new Valitron\Validator( $this->input );
-    $rules_to_map = $this->custom_message_rules( $form );
-    $this->valitron->mapFieldsRules( $rules_to_map );
+  public function create_valitron() {
+    $valitron = new Valitron\Validator( $this->input );
+    $rules_to_map = $this->custom_message_rules($valitron);
+    $valitron->mapFieldsRules( $rules_to_map );
+    return $valitron;
   }
 
   /**
    * Check for custom error messages in $form config
    * Add rules with custom messages individually to Valitron
    *
-   *
    * @since 0.5.0
    *
    * @param array $form Form configuration array
    * @return array Form rules that didn't have custom messages
    */
-   private function custom_message_rules( $form ) {
-    foreach( $form->rules as $field => $rules ) {
+   private function custom_message_rules($valitron) {
+    $rule_set = $this->rules;
+    $msg_set = $this->messages;
+
+    foreach( $rule_set as $field => $rules ) {
       // check if this field has any custom error msgs
-      if ( array_key_exists( $field, $form->messages ) ) {
-        foreach ( $form->messages[ $field ] as $rule => $message ) {
+      if ( array_key_exists( $field, $msg_set ) ) {
+        foreach ( $msg_set[ $field ] as $rule => $message ) {
           // add the rule with custom error message
-          $this->valitron->rule( $rule, $field )->message( $message );
-          // remove the rule from $form config or it will get mapped again later
+          $valitron->rule( $rule, $field )->message( $message );
+          // remove the rule from $lorem config or it will get mapped again later
           $key = array_search( $rule, $rules );
-          unset( $form->rules[ $field ][ $key ] );
+          unset( $rule_set[ $field ][ $key ] );
         }
       }
     }
-    return $form->rules;
+    return $rule_set;
   }
 }
