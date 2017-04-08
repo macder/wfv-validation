a86fb3f562b4f3c7aad5a2d704cf23a01fb9db0e
Maciej Derulski <macder7@gmail.com>
2017-04-08 14:32:53 -0400

    skeleton for new WFV_Input class

---
+++ class.wfv-input.php
@@ -0,0 +1,20 @@
+<?php defined( 'ABSPATH' ) or die();
+
+/**
+ *
+ *
+ *
+ * @since 0.7.2
+ */
+class WFV_Input {
+
+
+  /**
+   * _construct
+   *
+   * @param
+   */
+  function __construct( ) {
+  }
+
+}
