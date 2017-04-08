0bf10ca9bff44776b866cd36806427a0dc29059b
Maciej Derulski <macder7@gmail.com>
2017-04-07 22:07:08 -0400

    rules property now instance of WFV_Rules

--- class.wfv-form.php
+++ class.wfv-form.php
@@ -32,6 +32,7 @@ class WFV_Form extends WFV_Validate {
    * @access private
    */
   private function set_properties( $form ) {
+    $this->rules = new WFV_rules( $form['rules'] );
     $this->create_nonce_field();
     $this->catch_post();
   }
