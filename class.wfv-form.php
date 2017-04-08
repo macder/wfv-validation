bc822f04b8b01cbed33bf651662dae9913a681c0
Maciej Derulski <macder7@gmail.com>
2017-04-08 13:27:36 -0400

    moving getter methods into child class
    cleaner, better seperation of concern

--- class.wfv-form.php
+++ class.wfv-form.php
@@ -24,6 +24,54 @@ class WFV_Form extends WFV_Validate {
   }
 
   /**
+   * Return property value
+   *
+   * @since 0.6.1
+   * @param string $property Property key name
+   *
+   * @return string|array Property value
+   */
+  public function get( $property ) {
+    return ( true === property_exists( $this, $property ) ) ? $this->$property : null;
+  }
+
+  /**
+   * Return field value from $input property
+   *
+   * @since 0.6.1
+   * @param string $field Name of field
+   *
+   * @return string Field value
+   */
+  public function get_input( $field ) {
+    return $this->input[ $field ];
+  }
+
+  /**
+   * Return fields $error property
+   * By default returns all errors
+   * If $field_name is supplied a string, only error for the field
+   * $bag is array of messages, false returns first error as string
+   *
+   * @since 0.6.1
+   * @param string (optional) $field_name Only errors for $field_name
+   * @param bool (optional) $bag true return array error bag for field
+   *
+   * @return string|array String if $field is string and $bag = false, array otherwise
+   */
+  public function get_error( $field_name = null, $bag = false ) {
+    if( $field_name ) {
+      return ( true == $bag ) ? $this->errors[ $field_name ] : $this->errors[ $field_name ][0];
+    }
+    return $this->errors;
+  }
+
+
+  /*public function rule( $field ) {
+    return $this->rules->get( $field );
+  }*/
+
+  /**
    * Assign $form config to properties
    *
    * @since 0.4.0
