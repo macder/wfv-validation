e9bcb44701511df4d185c32b425a85ec9386ce9d
Maciej Derulski <macder7@gmail.com>
2017-04-08 16:53:57 -0400

    convienience method updated to support WFV_Input update

--- class.wfv-form.php
+++ class.wfv-form.php
@@ -55,7 +55,7 @@ class WFV_Form extends WFV_Validate {
    *
    * @return string Field value
    */
-  public function input( $field ) {
+  public function input( $field = null ) {
     return $this->input->get( $field );
   }
 
