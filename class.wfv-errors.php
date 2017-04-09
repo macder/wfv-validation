badc4eed712f77e725890dac2b1a739228b5040d
Maciej Derulski <macder7@gmail.com>
2017-04-08 21:18:28 -0400

    Don't decorate on instantiation
    If validation fails, it calls the set method

--- class.wfv-errors.php
+++ class.wfv-errors.php
@@ -13,9 +13,21 @@ class WFV_Errors {
    *
    * @param
    */
-  function __construct( $errors ) {
-    $this->set( $errors );
-    // print_r($this);
+  function __construct() {
+
+  }
+
+  /**
+   *
+   *
+   * @since 0.7.3
+   * @param array $errors Validation rules
+   * @access private
+   */
+  public function set( $errors ) {
+    foreach( $errors as $field => $error ) {
+      $this->$field = $error;
+    }
   }
 
   /**
@@ -27,19 +39,6 @@ class WFV_Errors {
    * @return string|array Property value
    */
   public function get( $property ) {
-    return ( true === property_exists( $this, $property ) ) ? $this->$property : null;
-  }
-
-  /**
-   *
-   *
-   * @since 0.7.3
-   * @param array $errors Validation rules
-   * @access private
-   */
-  private function set( $errors ) {
-    foreach( $errors as $field => $error ) {
-      $this->$field = $error;
-    }
+    return ( true === property_exists( $this, $property ) ) ? $this->$property : $this;
   }
 }
