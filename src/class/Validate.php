cc6a8f70b8245137a4088d6690085163f64ef5da
Maciej Derulski <macder7@gmail.com>
2017-04-12 03:37:04 -0400

    WFV\Validate - start porting to new structure

--- src/class/Validate.php
+++ src/class/Validate.php
@@ -9,6 +9,25 @@ defined( 'ABSPATH' ) or die();
  */
 class Validate implements Validation {
 
+  /**
+   * Form identifier
+   *
+   * @since 0.1.0
+   * @access protected
+   * @var string $action
+   */
+  // protected $action;
+
+  /**
+   * Validation rules
+   *
+   * @since 0.1.0
+   * @since 0.7.0 WFV_Rules instance
+   * @access protected
+   * @var class $rules Instance of WFV_Rules.
+   */
+  // protected $rules;
+
   use Accessor;
   use Mutator;
 
@@ -16,9 +35,21 @@ class Validate implements Validation {
    * __construct
    *
    * @since 0.8.0
+   * @param array $form
    *
    */
-  function __construct() {
+  function __construct( $action, Rules $rules, Input $input = null ) {
+    $this->action = $action;
+    $this->rules = $rules;
+  }
+
+  /**
+   *
+   * @param
+   * @since 0.8.0
+   *
+   */
+  public function rules() {
 
   }
 }
