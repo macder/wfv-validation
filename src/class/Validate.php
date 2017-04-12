3d4b5c4fc933b05b03187b663048bf90eceff803
Maciej Derulski <macder7@gmail.com>
2017-04-12 02:13:23 -0400

    WFV\Validate - remove action property
    still planning property locations

--- src/class/Validate.php
+++ src/class/Validate.php
@@ -13,15 +13,6 @@ class Validate implements Validation {
   use Mutator;
 
   /**
-   * Form identifier
-   *
-   * @since 0.1.0
-   * @access protected
-   * @var string $action
-   */
-  protected $action;
-
-  /**
    * __construct
    *
    * @since 0.8.0
