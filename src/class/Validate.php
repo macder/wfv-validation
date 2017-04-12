bb248cda95c428a70131be162448db78ea1ae975
Maciej Derulski <macder7@gmail.com>
2017-04-12 04:23:08 -0400

    WFV\Validate - bring over properties from legacy class

--- src/class/Validate.php
+++ src/class/Validate.php
@@ -16,7 +16,46 @@ class Validate implements Validation {
    * @access protected
    * @var string $action
    */
-  // protected $action;
+  protected $action;
+
+  /**
+   * Error message bag
+   *
+   * @since 0.6.1
+   * @since 0.7.3 WFV_Errors instance
+   * @access protected
+   * @var class $errors Instance of WFV_Errors.
+   */
+  protected $errors;
+
+  /**
+   * User input from failed validation
+   *
+   * @since 0.2.1
+   * @since 0.7.2 WFV_Input instance
+   * @access protected
+   * @var class $input Instance of WFV_Input.
+   */
+  protected $input;
+
+  /**
+   * Error message overrides
+   *
+   * @since 0.4.0
+   * @since 0.7.0 WFV_Messages instance
+   * @access protected
+   * @var class $messages Instance of WFV_Messages.
+   */
+  protected $messages;
+
+  /**
+   * Result from wp_nonce_field()
+   *
+   * @since 0.3.0
+   * @access protected
+   * @var string $nonce_field WP rendered nonce field.
+   */
+  protected $nonce_field;
 
   /**
    * Validation rules
