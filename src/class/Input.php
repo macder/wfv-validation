395035cd710156905ee51debdd2088653f9cf8d2
Maciej Derulski <macder7@gmail.com>
2017-04-12 03:37:34 -0400

    WFV\Input -  skeleton

---
+++ src/class/Input.php
@@ -0,0 +1,24 @@
+<?php
+namespace WFV;
+defined( 'ABSPATH' ) or die();
+
+/**
+ *
+ *
+ * @since 0.8.0
+ */
+class Input implements Validation {
+
+  use Accessor;
+  use Mutator;
+
+  /**
+   * __construct
+   *
+   * @since 0.8.0
+   *
+   */
+  function __construct() {
+
+  }
+}
