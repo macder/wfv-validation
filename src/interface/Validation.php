6dedd45a189f49c295298c39a265f4bcdf41c22e
Maciej Derulski <macder7@gmail.com>
2017-04-12 02:09:30 -0400

    new Validation instance, require basic access and mutate methods

---
+++ src/interface/Validation.php
@@ -0,0 +1,14 @@
+<?php
+namespace WFV;
+
+/**
+ *
+ *
+ * @since 0.8.0
+ */
+interface Validation {
+  public function get( $property );
+  public function has( $property );
+  public function put( $property, $value );
+  public function set( $properties );
+}
