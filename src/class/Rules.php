de5837f37c175ad5a205f49c52481046955b1e36
Maciej Derulski <macder7@gmail.com>
2017-04-12 22:20:52 -0400

    WFV\Rules - port in rule loading from legacy

--- src/class/Rules.php
+++ src/class/Rules.php
@@ -21,6 +21,38 @@ class Rules implements Validation {
   function __construct() {
 
   }
+
+  /**
+   * Push rules onto an instance of Valitron
+   *
+   * @since 0.7.0
+   * @param object $valitron Instance of Valitron\Validator
+   * @param object (optional) $messages Instance of WFV_Messages
+   */
+  public function load( &$valitron, $messages = null ) {
+
+    // loop the field
+    foreach( $this as $field => $rules ) {
+
+      // loop this field rules - a field can have many rules
+      foreach( $rules as $rule ) {
+        if( $this->is_custom( $rule ) ) {
+          $this->add( $rule, $valitron );
+        }
+
+        $valitron->rule( $rule, $field );
+
+        // check if this field/rule has a custom error message
+        /*if( $messages->has( $field, $rule ) ) {
+          $message = $messages->$field;
+          $valitron->rule( $rule, $field )->message( $message[ $rule ] );
+        } else { // use defaults
+          $valitron->rule( $rule, $field );
+        }*/
+      }
+    }
+  }
+
   /**
    * Check if rule is custom
    *
@@ -28,9 +60,9 @@ class Rules implements Validation {
    * @param string $rule
    * @access private
    *
-   * @return string|bool
+   * @return bool
    */
   private function is_custom( $rule ) {
-    return ( false !== strpos( $rule, 'custom:' ) ) ? $rule : false;
+    return ( false !== strpos( $rule, 'custom:' ) ) ? true: false;
   }
 }
