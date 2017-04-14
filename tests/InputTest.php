d216e63a80254882487cf85c7d75e048d7a4be88
Maciej Derulski <macder7@gmail.com>
2017-04-14 07:14:35 -0400

    phpunit - InputTest - now using dataProvider

--- tests/InputTest.php
+++ tests/InputTest.php
@@ -6,19 +6,26 @@ use WFV\Input;
 
 class InputTest extends \PHPUnit_Framework_TestCase {
 
-  public function test_Sanitize_Returns_Safe_Post() {
-
-    $_POST = array(
-      'action' => 'phpunit',
-      'name' => '<h1>hi</h1>'
-    );
-
-    $expected_result = array(
-      'action' => 'phpunit',
-      'name' => 'hi'
-    );
-
+  /**
+   * @param array $_POST
+   * @param array $expected_result
+   *
+   * @dataProvider provider_Test_Post
+   */
+  public function test_Sanitize_Returns_Safe_Post($post_array, $expected_result) {
     $result = get_object_vars( new Input('phpunit') );
     $this->assertEquals( $expected_result, $result );
+  }
+
+  public function provider_Test_Post() {
+    $_POST = array(
+      'action' => 'phpunit',
+      'name' => '<h1>Foo Bar</h1>'
+    );
+    $expected_result = array(
+      'action' => 'phpunit',
+      'name' => 'Foo Bar'
+    );
+    return array( array( $_POST, $expected_result ) );
   }
 }
