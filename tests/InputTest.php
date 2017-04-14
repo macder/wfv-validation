f2e6e2be56840688c4333dd3ac779d97dde477f8
Maciej Derulski <macder7@gmail.com>
2017-04-14 08:33:31 -0400

    phpunit - InputTest

--- tests/InputTest.php
+++ tests/InputTest.php
@@ -6,39 +6,59 @@ use WFV\Input;
 
 class InputTest extends \PHPUnit_Framework_TestCase {
 
+  const HTTP_POST = array(
+    'action' => 'phpunit',
+    'name' => '<h1>Foo Bar</h1>',
+    'skills' =>
+      array(
+        'js <script>console.log("oh no!");</script>',
+        'php <?php echo "oh no!"; ?>',
+      )
+  );
+
+  const EXPECTED_RESULT = array(
+    'action' => 'phpunit',
+    'name' => 'Foo Bar',
+    'skills' =>
+      array(
+        'js',
+        'php',
+      )
+  );
+
+  public function provider_test_post() {
+    $_POST = self::HTTP_POST;
+    $expected_result = self::EXPECTED_RESULT;
+    return array( array( $_POST, $expected_result ) );
+  }
+
   /**
-   * @param array $_POST
+   * Make sure properties are sanitized key/values from $_POST
+   *
+   * @param array $post The raw $_POST
    * @param array $expected_result
    *
    * @dataProvider provider_test_post
    */
-  public function test_post_is_sanitized_on_instance($post_array, $expected_result) {
+  public function test_post_gets_sanitized_to_instance( $post, $expected_result ) {
     $result = new Input('phpunit');
     $this->assertEquals( 'phpunit', $result->action );
-    $this->assertEquals( 'Mr. Foo Bar', $result->name );
-    $this->assertEquals( 'js', $result->skills[0]);
+    $this->assertEquals( 'Foo Bar', $result->name );
+    $this->assertEquals( 'js', $result->skills[0] );
 
     $result = get_object_vars( $result );
     $this->assertEquals( $expected_result, $result );
   }
 
-  public function provider_test_post() {
-    $_POST = array(
-      'action' => 'phpunit',
-      'name' => '<h1>Mr. Foo Bar</h1>',
-      'skills' => array(
-        'js <script>console.log("oh no!");</script>',
-        'php <?php echo "oh no!"; ?>',
-      )
-    );
-    $expected_result = array(
-      'action' => 'phpunit',
-      'name' => 'Mr. Foo Bar',
-      'skills' => array(
-        'js',
-        'php',
-      )
-    );
-    return array( array( $_POST, $expected_result ) );
+  /**
+   * Make sure POST is ignored when $_POST['action'] does not match $this->action
+   *
+   */
+  public function test_if_no_property_value_on_action_mismatch() {
+    $_POST = self::HTTP_POST;
+    $action = 'phpunit_manipulated';
+    $result = new Input( $action );
+
+    $this->assertFalse( ( property_exists( $result, 'name' ) ) );
   }
 }
