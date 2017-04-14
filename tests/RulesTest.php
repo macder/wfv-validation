2b179382c6413565917d144d1dcc56a911e5a87e
Maciej Derulski <macder7@gmail.com>
2017-04-14 17:39:51 -0400

    phpunit - rulesTest - mock form arguments as class constant

--- tests/RulesTest.php
+++ tests/RulesTest.php
@@ -6,4 +6,18 @@ use WFV\Rules;
 
 class RulesTest extends \PHPUnit_Framework_TestCase {
 
+  const FORM = array(
+    'action' => 'phpunit',
+
+    'rules'  => array(
+      'first_name' => ['required', 'alpha'],
+      'last_name' => ['required', 'alpha'],
+      'phone'      => ['required', 'custom:phone'],
+      'username'   => ['required'],
+      'password'   => ['required'],
+      'email'      => ['email','required'],
+      'website'    => ['required', 'url'],
+      'postal'     => ['custom:postal_code'],
+    )
+  );
 }
