a0aee736807da051fd9329448ddba3994842bd09
Maciej Derulski <macder7@gmail.com>
2017-04-14 01:57:57 -0400

    InputTest - skeleton class for Input unit tests

---
+++ tests/src/InputTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace WFV\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class InputTest extends TestCase {
+
+  public function testTrueIsTrue() {
+    $foo = true;
+    $this->assertTrue($foo);
+  }
+
+}
