a98d60aa73880be4856a70623b7c9c9493f90851
Maciej Derulski <macder7@gmail.com>
2017-04-14 07:53:55 -0400

    phpunit - InputTest - test array input, and value on props

--- tests/InputTest.php
+++ tests/InputTest.php
@@ -10,21 +10,34 @@ class InputTest extends \PHPUnit_Framework_TestCase {
    * @param array $_POST
    * @param array $expected_result
    *
-   * @dataProvider provider_Test_Post
+   * @dataProvider provider_test_post
    */
-  public function test_Sanitize_Returns_Safe_Post($post_array, $expected_result) {
-    $result = get_object_vars( new Input('phpunit') );
+  public function test_post_is_sanitized_on_instance($post_array, $expected_result) {
+    $result = new Input('phpunit');
+    $this->assertEquals( 'phpunit', $result->action );
+    $this->assertEquals( 'Mr. Foo Bar', $result->name );
+    $this->assertEquals( 'js', $result->skills[0]);
+
+    $result = get_object_vars( $result );
     $this->assertEquals( $expected_result, $result );
   }
 
-  public function provider_Test_Post() {
+  public function provider_test_post() {
     $_POST = array(
       'action' => 'phpunit',
-      'name' => '<h1>Foo Bar</h1>'
+      'name' => '<h1>Mr. Foo Bar</h1>',
+      'skills' => array(
+        'js <script>console.log("oh no!");</script>',
+        'php <?php echo "oh no!"; ?>',
+      )
     );
     $expected_result = array(
       'action' => 'phpunit',
-      'name' => 'Foo Bar'
+      'name' => 'Mr. Foo Bar',
+      'skills' => array(
+        'js',
+        'php',
+      )
     );
     return array( array( $_POST, $expected_result ) );
   }
