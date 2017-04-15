7851ec67a0d6ff8f4798a510579f841134a6320f
Maciej Derulski <macder7@gmail.com>
2017-04-15 06:11:56 -0400

    phpunit - FormTest - mock $_POST data

--- tests/FormTest.php
+++ tests/FormTest.php
@@ -7,6 +7,22 @@ use WFV\Input;
 
 class FormTest extends \PHPUnit_Framework_TestCase {
 
+  const HTTP_POST = array(
+    'action' => 'phpunit',
+    'name' => 'Foo Bar',
+    'skills' => array(
+      'js',
+      'php',
+      'sh',
+      'sass',
+    ),
+    'color' => array(
+      'red',
+      'blue',
+      'green'
+    )
+  );
+
   /**
    *
    *
