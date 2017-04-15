5d334ad7dce5f011487d5517d21412e668692ec8
Maciej Derulski <macder7@gmail.com>
2017-04-14 20:51:04 -0400

    phpunit - Rules - start the blueprint - still rough

--- tests/RulesTest.php
+++ tests/RulesTest.php
@@ -6,18 +6,64 @@ use WFV\Rules;
 
 class RulesTest extends \PHPUnit_Framework_TestCase {
 
-  const FORM = array(
-    'action' => 'phpunit',
-
-    'rules'  => array(
-      'first_name' => ['required', 'alpha'],
-      'last_name' => ['required', 'alpha'],
-      'phone'      => ['required', 'custom:phone'],
-      'username'   => ['required'],
-      'password'   => ['required'],
-      'email'      => ['email','required'],
-      'website'    => ['required', 'url'],
-      'postal'     => ['custom:postal_code'],
-    )
+  const RULES = array(
+    'first_name' => ['required', 'alpha'],
+    'last_name' => ['required', 'alpha'],
+    'phone'      => ['required', 'custom:phone'],
+    'username'   => ['required'],
+    'password'   => ['required'],
+    'email'      => ['email','required'],
+    'website'    => ['required', 'url'],
+    'postal'     => ['custom:postal_code'],
   );
+
+  /**
+   *
+   *
+   */
+  public function test_rules_get_set_as_properties() {
+    $rules = new Rules();
+    $rules->set( self::RULES );
+
+    foreach( $rules as $field => $ruleset ) {
+      $this->assertTrue( property_exists( $rules, $field ) );
+    }
+  }
+
+  /**
+   *
+   *
+   */
+  public function test_rules_if_field_has_rules() {
+    $rules = new Rules();
+    $rules->set( self::RULES );
+
+    foreach( $rules as $field => $ruleset ) {
+
+      foreach( $ruleset as $index => $rule ) {
+
+        $has_rule = ( $rule === self::RULES[$field][$index] ) ? true : false;
+
+        $this->assertTrue( $has_rule );
+      }
+    }
+  }
+
+  /**
+   *
+   *
+   */
+  public function test_rule_is_custom_returns_true() {
+    $rules = new Rules();
+    $this->assertTrue( $rules->is_custom('custom:phone') );
+  }
+
+  /**
+   *
+   *
+   */
+  public function test_rule_is_custom_returns_false() {
+    $rules = new Rules();
+    $this->assertFalse( $rules->is_custom('required') );
+  }
 }
