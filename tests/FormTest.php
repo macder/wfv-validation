d87af0008979ca7e6d360dc6846d565f68d9b4a7
Maciej Derulski <macder7@gmail.com>
2017-04-15 02:22:06 -0400

    phpunit - Form - testing skeleton

---
+++ tests/FormTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace WFV;
+
+use WFV\Form;
+use WFV\Input;
+
+class FormTest extends \PHPUnit_Framework_TestCase {
+
+  /**
+   *
+   *
+   */
+  public function test_form() {
+
+  }
+
+}
