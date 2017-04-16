d515d78237e97fa0cecd56d1c0ed2dd6402f07b9
Maciej Derulski <macder7@gmail.com>
2017-04-15 20:10:00 -0400

    phpunit - ValidatorTest - basic start - this could be a big one

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -7,37 +7,57 @@ use WFV\Factory\ValidationFactory;
 class ValidatorTest extends \PHPUnit_Framework_TestCase {
 
   const FORM = array(
-    'action'  => 'contact_form', // unique identifier
+    'action'  => 'phpunit', // unique identifier
     'rules'   => array(
-      'title'     => ['required'],
-      'name'      => ['required'],
-      //'phone'     => ['required', 'custom:phone'],
-      'email'     => ['required', 'email'],
-      //'postal'    => ['custom:postal_code'],
-      'website'   => ['required', 'url'],
-      // 'msg'       => ['required']
-    ),
-
-    // override an error msg
-    'messages' => [
-      'email' => array(
-        'required' => 'Your email is required so we can reply back'
-      )
-    ]
+      'name'      => ['required']
+    )
   );
 
+  const HTTP_POST = array(
+    'action' => 'phpunit',
+    'name' => 'Foo Bar'
+  );
+
+  protected static $form_no_post;
+
+  protected static $form_has_post;
+
+  // there are a lot of possible cases
+  // planning the automation...
+
+  public static function setUpBeforeClass() {
+    $form_no_post = self::FORM;
+    ValidationFactory::create( $form_no_post );
+    self::$form_no_post = $form_no_post;
+
+    $_POST = self::HTTP_POST;
+    ValidationFactory::create( $form_has_post );
+    self::$form_has_post = $form_has_post;
+  }
+
   /**
-   *
+   * is the factory building all instances and assigning
+   *  them as properties to WFV\Validator when there is no $_POST?
    *
    */
-  public function test_validator_factory_create() {
-    $form = self::FORM;
-    ValidationFactory::create( $form );
+  public function test_validator_factory_create_with_no_post() {
+    $this->assertInstanceOf( 'WFV\Validator', self::$form_no_post );
+    $this->assertInstanceOf( 'WFV\Errors', self::$form_no_post->errors );
+    $this->assertInstanceOf( 'WFV\Input', self::$form_no_post->input );
+    $this->assertInstanceOf( 'WFV\Messages', self::$form_no_post->messages );
+    $this->assertInstanceOf( 'WFV\Rules', self::$form_no_post->rules );
+  }
 
-    $this->assertInstanceOf( 'WFV\Validator', $form );
-    $this->assertInstanceOf( 'WFV\Errors', $form->errors );
-    $this->assertInstanceOf( 'WFV\Input', $form->input );
-    $this->assertInstanceOf( 'WFV\Messages', $form->messages );
-    $this->assertInstanceOf( 'WFV\Rules', $form->rules );
+  /**
+   * $_POST exists - is the factory building all instances and assigning
+   *  them as properties to WFV\Validator?
+   *
+   */
+  public function test_validator_factory_create_with_post() {
+    $this->assertInstanceOf( 'WFV\Validator', self::$form_has_post );
+    $this->assertInstanceOf( 'WFV\Errors', self::$form_has_post->errors );
+    $this->assertInstanceOf( 'WFV\Input', self::$form_has_post->input );
+    $this->assertInstanceOf( 'WFV\Messages', self::$form_has_post->messages );
+    $this->assertInstanceOf( 'WFV\Rules', self::$form_has_post->rules );
   }
 }
