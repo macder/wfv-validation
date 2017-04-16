2feb567e65c0eeb07eebe9e0d317336eb1edbc2a
Maciej Derulski <macder7@gmail.com>
2017-04-15 21:33:40 -0400

    phpunit - FormTest - fixes useless and skipped tests

--- tests/FormTest.php
+++ tests/FormTest.php
@@ -23,75 +23,83 @@ class FormTest extends \PHPUnit_Framework_TestCase {
     )
   );
 
+  /**
+   * Instance of WFV\Validator
+   *
+   * @access protected
+   * @var WFV\Validator $form
+   */
+  protected static $form;
+
+  /**
+   * Create WFV\Form instance with WFV\Input input property
+   *
+   */
   public static function setUpBeforeClass() {
     $_POST = self::HTTP_POST;
+
+    $action = $_POST['action'];
+    self::$form = new Form();
+    $input = new Input( $action );
+    self::$form->put( 'action', $action );
+    self::$form->put( 'input', $input );
   }
 
+  /**
+   *
+   *
+   */
   public static function tearDownAfterClass() {
     $_POST = null;
   }
 
   /**
-   * Create WFV\Form instance and put WFV\Input on input property
+   *
    *
    */
-  public function test_form_create() {
-    $action = $_POST['action'];
-    $form = new Form();
-    $input = new Input( $action );
-    $form->put( 'action', $action );
-    $form->put( 'input', $input );
-
-    return $form;
-  }
-
-  /**
-   *
-   * @depends test_form_create
-   */
-  public function test_form_input_prop_has_props_from_POST( $form ) {
-    foreach( $form->input as $key => $value ) {
-      $this->assertTrue( property_exists( $form->input, $key ) );
+  public function test_form_input_prop_has_props_from_POST() {
+    foreach( self::$form->input as $key => $value ) {
+      $this->assertTrue( property_exists( self::$form->input, $key ) );
     }
   }
 
   /**
    *
-   * @depends test_form_create
+   *
    */
-  public function test_form_does_checked_if_return_checked( $form ) {
-    foreach( $form->input->skills as $skill ) {
-      $this->assertEquals( 'checked', $form->checked_if( 'skills', $skill ) );
+  public function test_form_does_checked_if_return_checked() {
+    foreach( self::$form->input->skills as $skill ) {
+      $this->assertEquals( 'checked', self::$form->checked_if( 'skills', $skill ) );
     }
   }
 
   /**
    *
-   * @depends test_form_create
+   *
    */
-  public function test_form_does_checked_if_return_null( $form ) {
-    foreach( $form->input->skills as $skill ) {
-      $this->assertEquals( null, $form->checked_if( 'skills', 'BAH!' ) );
+  public function test_form_does_checked_if_return_null() {
+    foreach( self::$form->input->skills as $skill ) {
+      $this->assertEquals( null, self::$form->checked_if( 'skills', 'BAH!' ) );
     }
   }
 
   /**
    *
-   * @depends test_form_create
+   *
    */
-  public function test_form_does_selected_if_return_selected( $form ) {
-    foreach( $form->input->skills as $skill ) {
-      $this->assertEquals( 'selected', $form->selected_if( 'skills', $skill ) );
+  public function test_form_does_selected_if_return_selected() {
+    foreach( self::$form->input->skills as $skill ) {
+      $this->assertEquals( 'selected', self::$form->selected_if( 'skills', $skill ) );
     }
   }
 
   /**
    *
-   * @depends test_form_create
+   *
    */
-  public function test_form_does_selected_if_return_null( $form ) {
-    foreach( $form->input->skills as $skill ) {
-      $this->assertEquals( null, $form->selected_if( 'skills', 'BAH!' ) );
+  public function test_form_does_selected_if_return_null() {
+    foreach( self::$form->input->skills as $skill ) {
+      $this->assertEquals( null, self::$form->selected_if( 'skills', 'BAH!' ) );
     }
   }
 }
