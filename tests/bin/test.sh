237a7bff005c15a32cc2f83a7f88fb50940b0345
Maciej Derulski <macder7@gmail.com>
2017-04-17 01:15:12 -0400

    tests - bin - run the full suite here - for now

--- tests/bin/test.sh
+++ tests/bin/test.sh
@@ -9,4 +9,4 @@ mapfile -t CONFIG < config
 root_path=${CONFIG[1]} # line 2 in ./config
 
 cd $root_path
-vendor/bin/phpunit tests/FormTest --report-useless-tests --verbose
+vendor/bin/phpunit --report-useless-tests --verbose
