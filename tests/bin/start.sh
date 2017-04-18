2960b19903f145c2415bbe0590e172069a060cd7
Maciej Derulski <macder7@gmail.com>
2017-04-18 18:32:08 -0400

    tests/bin - start - remove old code

--- tests/bin/start.sh
+++ tests/bin/start.sh
@@ -39,17 +39,5 @@ function user_param_input {
   input=${input:-$2}
 }
 
-# DB_NAME=$1
-# DB_USER=$2
-# DB_PASS=$3
-# DB_HOST=${4-localhost}
-# WP_VERSION=${5-latest}
-# SKIP_DB_CREATE=${6-false}
 
-for i in ${!DEFAULT[*]} ; do
 
-  default=${DEFAULT[$i]}
-  user_confirm "$i" "$default"
-  DEFAULT[$i]=$input
-  echo ${DEFAULT[$i][*]}
-done
