ba65029fe4ed9bc78bb3b4e653746e6d8f6ecd98
Maciej Derulski <macder7@gmail.com>
2017-04-17 20:27:06 -0400

    travis - debugging

--- bin/travis.sh
+++ bin/travis.sh
@@ -4,4 +4,6 @@ yell() { echo "$0: $*" >&2; }
 die() { yell "$*"; exit 111; }
 try() { "$@" || die "cannot $*"; }
 
-echo "Travis PHP: "${TRAVIS_PHP_VERSION:0:2}
+echo "Travis PHP: travis.sh w0w0w"
+
+# ${TRAVIS_PHP_VERSION:0:2}
