20f73999e67f95defbbd6a48722a0990d13350f5
Maciej Derulski <macder7@gmail.com>
2017-04-17 20:47:20 -0400

    travis - moving bash stuff into travis.sh

--- bin/travis.sh
+++ bin/travis.sh
@@ -4,7 +4,15 @@ yell() { echo "$0: $*" >&2; }
 die() { yell "$*"; exit 111; }
 try() { "$@" || die "cannot $*"; }
 
-echo "test test test test test test test test test  "
-php --version | head -n 1 | cut -d " " -f 2 | cut -c 1,3
+
+php_version=$(php --version | head -n 1 | cut -d " " -f 2 | cut -c 1,3);
+
+if [ 70 = "$php_version" ]; then
+  composer global require "phpunit/phpunit=5.7.*"
+else
+  composer global require "phpunit/phpunit=4.8.*"
+fi
+
+composer require "vlucas/valitron=^1.4"
 
 # ${TRAVIS_PHP_VERSION:0:2}
