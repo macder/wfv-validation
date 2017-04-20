c4cac7716958cc414fef15c4d76cd06edcc61a97
Maciej Derulski <macder7@gmail.com>
2017-04-20 12:27:59 -0400

    bash - start - almost optimized and documented

--- bin/lib/run_test.sh
+++ bin/lib/run_test.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
-TEST_NAME="tests/"$1
+TEST_NAME=$1
+TEST_PATH="tests/"$TEST_NAME
 
-# Run test suite
-#
-#
-yell() { echo "$0: $*" >&2; }
-die() { yell "$*"; exit 111; }
-try() { "$@" || die "cannot $*"; }
-
-mapfile -t CONFIG < config.local
-
-# git update-index --assume-unchanged config.local
-
-root_path=${CONFIG[1]} # line 2 in ./config
-
-# check if path was set in config.local
-if [ ! -d "$root_path" ]; then
-  die "The PATH defined in config.local does not exist!"
+if [ "" == "$TEST_NAME" ] ; then
+  TEST_NAME="All"
 fi
 
-cd $root_path
-vendor/bin/phpunit $TEST_NAME --report-useless-tests --verbose
+echo "Ok, Running $TEST_NAME"
+echo ""
+
+cd ..
+vendor/bin/phpunit $TEST_PATH --report-useless-tests --verbose
