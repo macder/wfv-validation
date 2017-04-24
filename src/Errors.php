670a1ebfea956969e752b005390cd7d2030784fb
Maciej Derulski <macder7@gmail.com>
2017-04-23 20:42:17 -0400

    Errors - moved 'error' method from Validator into here - now first()

--- src/Errors.php
+++ src/Errors.php
@@ -20,4 +20,17 @@ class Errors implements ValidationInterface {
    */
   function __construct() {
   }
+
+  /**
+   * Convienience method to get first error on field.
+   *
+   * @since 0.9.1
+   *
+   * @param string $field Name of field
+   * @return string First error message.
+   */
+  public function first( $field ) {
+    $errors = $this->$field;
+    return $errors[0];
+  }
 }
