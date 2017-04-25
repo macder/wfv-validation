c56ea9a81aec5d5f8bf336556d3aca18904ef516
Maciej Derulski <macder7@gmail.com>
2017-04-25 00:48:24 -0400

    FormTest - updated after code refactor

--- tests/FormTest.php
+++ tests/FormTest.php
@@ -2,18 +2,30 @@
 
 namespace WFV;
 
+use WFV\Errors;
 use WFV\Form;
+use WFV\Guard;
 use WFV\Input;
+use WFV\Messages;
+use WFV\Rules;
 
 class FormTest extends \PHPUnit_Framework_TestCase {
 
   /**
-   * Instance of WFV\Form
+   * Instance of WFV\Form before $_POST.
    *
    * @access protected
-   * @var WFV\Form $form
+   * @var WFV\Form $form_before_post
    */
-  protected static $form;
+  protected static $form_before_post;
+
+  /**
+   * Instance of WFV\Form after $_POST.
+   *
+   * @access protected
+   * @var WFV\Form $form_after_post
+   */
+  protected static $form_after_post;
 
   /**
    *
@@ -24,13 +36,37 @@ class FormTest extends \PHPUnit_Framework_TestCase {
   protected static $http_post;
 
   /**
+   *
+   *
+   * @access protected
+   * @var
+   */
+  protected static $instances;
+
+  /**
    * Create WFV\Form instance with WFV\Input input property
    *
    */
   public static function setUpBeforeClass() {
+    $form_args = array(
+      'action'  => 'phpunit',
+      'rules'   => array(
+        'name'      => ['required'],
+        'email'     => ['required', 'email'],
+        'shades'   => ['required'],
+        'colors'   => ['required'],
+      ),
+      'messages' => [
+        'email' => array(
+          'required' => 'Email required - phpunit'
+        ),
+      ]
+    );
+
     self::$http_post = array(
       'action' => 'phpunit',
       'name' => 'Foo Bar',
+      'email' => 'foo@bar.com',
       'shades' => array(
         'lightest',
         'light',
@@ -44,13 +80,26 @@ class FormTest extends \PHPUnit_Framework_TestCase {
       )
     );
 
-    $_POST = self::$http_post;
+    self::$instances = array(
+      'errors' => 'WFV\Errors',
+      'input' => 'WFV\Input',
+      'messages' => 'WFV\Messages',
+      'rules' => 'WFV\Rules',
+    );
 
-    $action = $_POST['action'];
-    self::$form = new Form();
+    $action = $form_args['action'];
+    $errors = new Errors();
     $input = new Input( $action );
-    self::$form->put( 'action', $action );
-    self::$form->put( 'input', $input );
+    $messages = new Messages( $form_args['messages'] );
+    $rules = new Rules( $form_args['rules'] );
+
+    self::$form_before_post = new Form( $action, $input, $rules, $messages, $errors );
+
+    $_POST = self::$http_post;
+    $input = new Input( $action );
+
+    self::$form_after_post = new Form( $action, $input, $rules, $messages, $errors );
+
   }
 
   /**
@@ -66,29 +115,32 @@ class FormTest extends \PHPUnit_Framework_TestCase {
    *
    */
   public function test_form_is_instance() {
-    $this->assertInstanceOf( 'WFV\Form', self::$form );
+    $this->assertInstanceOf( 'WFV\Form', self::$form_before_post );
+    $this->assertInstanceOf( 'WFV\Form', self::$form_after_post );
   }
 
   /**
-   * Is the form input property an instance of WFV\Input?
+   *
    *
    */
-  public function test_form_input_property_is_input_instance() {
-    $this->assertInstanceOf( 'WFV\Input', self::$form->input );
-  }
-
-  /**
-   * Did the WFV\Input instance create properties from POST keys?
-   *
-   */
-  public function test_form_input_prop_has_props_from_POST() {
-    $post = self::$http_post;
-    // array_push($post, array('injected_field' => 'injected_value'));
-
-    foreach( $post as $key => $value ) {
-      $this->assertTrue( property_exists( self::$form->input, $key ) );
+  public function test_form_has_instances_before_post() {
+    $instances = self::$instances;
+    foreach( $instances as $property_name => $expected_instance ) {
+      $this->assertInstanceOf( $expected_instance, self::$form_before_post->$property_name );
     }
   }
+
+  /**
+   *
+   *
+   */
+  public function test_form_has_instances_after_post() {
+    $instances = self::$instances;
+    foreach( $instances as $property_name => $expected_instance ) {
+      $this->assertInstanceOf( $expected_instance, self::$form_after_post->$property_name );
+    }
+  }
+
 
   /**
    * Does 'checked_if' method return 'checked' when POST data
@@ -96,8 +148,8 @@ class FormTest extends \PHPUnit_Framework_TestCase {
    *
    */
   public function test_form_does_checked_if_return_checked() {
-    foreach( self::$form->input->shades as $shade ) {
-      $this->assertEquals( 'checked', self::$form->checked_if( 'shades', $shade ) );
+    foreach( self::$form_after_post->input->shades as $shade ) {
+      $this->assertEquals( 'checked', self::$form_after_post->checked_if( 'shades', $shade ) );
     }
   }
 
@@ -107,8 +159,8 @@ class FormTest extends \PHPUnit_Framework_TestCase {
    *
    */
   public function test_form_does_checked_if_return_null() {
-    foreach( self::$form->input->shades as $shade ) {
-      $this->assertEquals( null, self::$form->checked_if( 'shades', 'BAH!' ) );
+    foreach( self::$form_after_post->input->shades as $shade ) {
+      $this->assertEquals( null, self::$form_after_post->checked_if( 'shades', 'BAH!' ) );
     }
   }
 
@@ -118,8 +170,8 @@ class FormTest extends \PHPUnit_Framework_TestCase {
    *
    */
   public function test_form_does_selected_if_return_selected() {
-    foreach( self::$form->input->shades as $shade ) {
-      $this->assertEquals( 'selected', self::$form->selected_if( 'shades', $shade ) );
+    foreach( self::$form_after_post->input->shades as $shade ) {
+      $this->assertEquals( 'selected', self::$form_after_post->selected_if( 'shades', $shade ) );
     }
   }
 
@@ -129,8 +181,37 @@ class FormTest extends \PHPUnit_Framework_TestCase {
    *
    */
   public function test_form_does_selected_if_return_null() {
-    foreach( self::$form->input->shades as $shade ) {
-      $this->assertEquals( null, self::$form->selected_if( 'shades', 'BAH!' ) );
+    foreach( self::$form_after_post->input->shades as $shade ) {
+      $this->assertEquals( null, self::$form_after_post->selected_if( 'shades', 'BAH!' ) );
     }
+  }
+
+  /**
+   *
+   *
+   */
+  public function test_form_must_validate_return_true() {
+    $result = self::$form_after_post->must_validate();
+    $this->assertTrue($result);
+  }
+
+  /**
+   *
+   *
+   */
+  public function test_form_must_validate_return_false_if_no_post() {
+    $result = self::$form_before_post->must_validate();
+    $this->assertFalse($result);
+  }
+
+  /**
+   *
+   *
+   */
+  public function test_form_must_validate_return_false_if_action_mismatch() {
+    $form = self::$form_after_post;
+    $form->input->put('action', 'tampered_action');
+    $result = $form->must_validate();
+    $this->assertFalse($result);
   }
 }
