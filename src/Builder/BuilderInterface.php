2198bfe9095f162b8640d06c024be42f4ef36c3b
Maciej Derulski <macder7@gmail.com>
2017-04-26 00:29:24 -0400

    BuilderInterface - starting point

---
+++ src/Builder/BuilderInterface.php
@@ -0,0 +1,9 @@
+<?php
+namespace WFV\Builder;
+defined( 'ABSPATH' ) or die();
+
+interface BuilderInterface {
+	public function create();
+	public function add_rules();
+	public function add_input();
+}
