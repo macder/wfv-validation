1fe15ee850272ce2e5f7898a8555f2709e9ebd3a
Maciej Derulski <macder7@gmail.com>
2017-04-26 00:30:06 -0400

    BuilderInterface - formatting

--- src/Builder/BuilderInterface.php
+++ src/Builder/BuilderInterface.php
@@ -3,7 +3,7 @@ namespace WFV\Builder;
 defined( 'ABSPATH' ) or die();
 
 interface BuilderInterface {
-	public function create();
-	public function add_rules();
-	public function add_input();
+  public function create();
+  public function add_rules();
+  public function add_input();
 }
