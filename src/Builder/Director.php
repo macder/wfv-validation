5f8dfb7aa26880758754a4c8ed9e51c438340bb9
Maciej Derulski <macder7@gmail.com>
2017-04-27 11:52:11 -0400

    Director - rework for fluent building

--- src/Builder/Director.php
+++ src/Builder/Director.php
@@ -5,23 +5,55 @@ defined( 'ABSPATH' ) or die();
 /**
  *
  *
- * @since 0.9.2
+ * @since 0.10.0
  *
  */
 class Director {
 
+	private $config = [];
+
 	/**
 	 *
 	 *
-	 * @since 0.9.2
+	 * @since 0.10.0
 	 *
-	 * @param
-	 * @return
+	 * @param BuilderInterface $builder
+	 * @return class
 	 */
 	public function build( BuilderInterface $builder ) {
-		$builder->rules()
-			->input()
-			->create();
-		return $builder->result();
+		foreach( $this->config['component'] as $entity => $attributes ) {
+			$builder->$entity( $attributes );
+		}
+		$builder->create( $this->config['action'] );
+
+		return $builder->deliver();
+	}
+
+	/**
+	 *
+	 *
+	 * @since 0.10.0
+	 *
+	 * @param string $type
+	 * @param string $attribute
+	 * @return
+	 */
+	public function give_attribute( $type, $attribute ) {
+		$this->config[ $type ] = $attribute ;
+		return $this;
+	}
+
+	/**
+	 *
+	 *
+	 * @since 0.10.0
+	 *
+	 * @param string $entity
+	 * @param string|array $attributes
+	 * @return
+	 */
+	public function with_entity( $entity, $attributes ) {
+		$this->config['component'][$entity] = $attributes;
+		return $this;
 	}
 }
