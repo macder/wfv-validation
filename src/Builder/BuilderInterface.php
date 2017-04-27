5d4cff9b9f25babb1a105cde4945abfb3f8e610f
Maciej Derulski <macder7@gmail.com>
2017-04-27 02:40:17 -0400

    BuilderInterface - phpdoc, files cleanup

--- src/Builder/BuilderInterface.php
+++ src/Builder/BuilderInterface.php
@@ -3,7 +3,14 @@ namespace WFV\Builder;
 defined( 'ABSPATH' ) or die();
 
 interface BuilderInterface {
+
+  /**
+   * @return mixed
+   */
   public function create();
-  public function add_rules();
-  public function add_input();
+
+  /**
+   * @return mixed
+   */
+  public function result();
 }
