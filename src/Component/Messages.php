752a5f45aae5553093b80c32a498b6c8c8760b67
Maciej Derulski <macder7@gmail.com>
2017-04-27 07:27:06 -0400

    Messages - prepare for integration

--- src/Component/Messages.php
+++ src/Component/Messages.php
@@ -7,37 +7,32 @@ defined( 'ABSPATH' ) or die();
  *
  * @since 0.8.0
  */
-class Messages implements ValidationInterface {
+class Messages {
 
-  use AccessorTrait;
-  use MutatorTrait;
+	/**
+	 * __construct
+	 *
+	 * @since 0.8.0
+	 *
+	 * @param array (optional) $messages
+	 */
+	function __construct( $messages = null ) {
 
-  /**
-   * __construct
-   *
-   * @since 0.8.0
-   *
-   * @param array (optional) $messages
-   */
-  function __construct( $messages = null ) {
-    if( $messages ) {
-      $this->set( $messages );
-    }
-  }
+	}
 
-  /**
-   * Check if a fields rule has custom message
-   *
-   * @since 0.7.0
-   *
-   * @param string $field
-   * @param string $rule
-   * @return bool
-   */
-  public function exist( $field, $rule ) {
-    if( property_exists( $this, $field ) && array_key_exists( $rule, $this->$field ) ) {
-      return true;
-    }
-    return false;
-  }
+	/**
+	 * Check if a fields rule has custom message
+	 *
+	 * @since 0.7.0
+	 *
+	 * @param string $field
+	 * @param string $rule
+	 * @return bool
+	 */
+	public function exist( $field, $rule ) {
+		if( property_exists( $this, $field ) && array_key_exists( $rule, $this->$field ) ) {
+			return true;
+		}
+		return false;
+	}
 }
