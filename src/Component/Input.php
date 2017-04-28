071add97d17972958fa7d3724711611f1b613039
Maciej Derulski <macder7@gmail.com>
2017-04-28 06:08:52 -0400

    Input - now a collection - related logic moved to collection abstract

--- src/Component/Input.php
+++ src/Component/Input.php
@@ -2,14 +2,23 @@
 namespace WFV\Component;
 defined( 'ABSPATH' ) or die();
 
-use WFV\Contract\ValidationInterface;
+use WFV\Helper\Collection;
 
 /**
  *
  *
  * @since 0.8.0
  */
-class Input implements ValidationInterface {
+class Input extends Collection {
+
+	/**
+	 * Input data from a form
+	 *
+	 * @since 0.10.0
+	 * @access private
+	 * @var array
+	 */
+	protected $data = array();
 
 	/**
 	 * __construct
@@ -20,65 +29,8 @@ class Input implements ValidationInterface {
 	 */
 	function __construct( $action ) {
 		if( $this->is_submit( $action ) ) {
-			$this->copy_input();
+			$this->assign_input();
 		}
-	}
-
-	/**
-	 *
-	 *
-	 * @since 0.9.0
-	 *
-	 * @param string $field
-	 * @param string|array (optional) $callback
-	 * @return string|null
-	 */
-	public function render( $field, callable $callback ) {
-		if( is_string( $field ) ) {
-			return ( $this->has( $field ) ) ? $this->call_func( $callback, $this->$field ) : null;
-		}
-		return null;
-	}
-
-	/**
-	 * Transform a string or array leafs using a callback.
-	 * When $value is an array, only the leafs (no keys) are transformed.
-	 * Infinite array traversal.
-	 *
-	 * @since 0.9.0
-	 *
-	 * @param string|array $value
-	 * @param string $callback
-	 * @return string|array
-	 */
-	public function transform( $input, callable $callback ) {
-		if( is_array( $input ) ) {
-			return $this->transform_array_leafs( $input, $callback );
-		}
-		return $this->call_func( $callback, $input );
-	}
-
-	/**
-	 * Trigger a callback function
-	 *
-	 * @since 0.9.0
-	 * @access private
-	 *
-	 * @param string|array $callback
-	 * @param string (optional) $input The input string
-	 * @return
-	 */
-	private function call_func( $callback, $input = null ) {
-		if( is_array( $callback ) ) {
-			$method = $callback[0];
-			$args = $callback[1];
-
-			if ( $input ) {
-				array_unshift( $args, $input );
-			}
-			return call_user_func_array( $method, $args );
-		}
-		return $callback( $input );
 	}
 
 	/**
@@ -87,43 +39,12 @@ class Input implements ValidationInterface {
 	 * @since 0.9.0
 	 * @access private
 	 */
-	private function copy_input() {
+	protected function assign_input() {
 		// WIP reduce responsibility
 		$input = $this->transform_array_leafs( $_POST, 'stripslashes' );
-		// $input = $_POST;
-
 		foreach( $input as $field => $value ) {
-			$this->$field = $value;
+			$this->data[ $field ] = $value;
 		}
-	}
-
-	/**
-	 * Check if property exists
-	 *
-	 * @since 0.8.0
-	 *
-	 * @param string $property
-	 * @return bool
-	 */
-	private function has( $property ) {
-		return ( true === property_exists( $this, $property ) ) ? true : false;
-	}
-
-	/**
-	 * Transform the $array leafs, traversing infinite dimensions
-	 *
-	 * @since 0.9.0
-	 * @access private
-	 *
-	 * @param array $array
-	 * @param string|array $callback
-	 * @return array
-	 */
-	private function transform_array_leafs( $array, $callback ) {
-		array_walk_recursive( $array, function( &$item, $key ) use( $callback ) {
-			$item = $this->call_func( $callback, $item );
-		} );
-		return $array;
 	}
 
 	/**
