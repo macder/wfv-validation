ff1e9613ac9f35f76b0405d040bcc23bf86b225e
Maciej Derulski <macder7@gmail.com>
2017-04-28 08:16:14 -0400

    InputTest - Should cover this class for now

--- tests/src/Component/InputTest.php
+++ tests/src/Component/InputTest.php
@@ -5,4 +5,81 @@ use WFV\Component\Input;
 
 class InputTest extends \PHPUnit_Framework_TestCase {
 
+	/**
+	 *
+	 *
+	 * @access protected
+	 * @var
+	 */
+	protected static $http_post;
+
+	/**
+	 *
+	 *
+	 */
+  protected function setUp() {
+    self::$http_post = array(
+      'action' => 'phpunit',
+      'name' => 'Foo Bar',
+      'email' => 'foo@bar.com',
+      'shades' => array(
+        'lightest',
+        'light',
+        'dark',
+        'darkest',
+      ),
+      'color' => array(
+        'red',
+        'green',
+        'blue',
+      )
+    );
+	}
+
+	/**
+	 *
+	 *
+	 */
+	protected function tearDown() {
+		self::$http_post = null;
+		$_POST = null;
+	}
+
+	/**
+	 *
+	 *
+	 */
+	public function testInputIsInstanceOfInputBeforePost() {
+		$result = new Input('phpunit');
+		$this->assertInstanceOf( Input::class, $result );
+	}
+
+	/**
+	 *
+	 *
+	 */
+	public function testInputIsInstanceOfInputAfterPost() {
+		$_POST = self::$http_post;
+		$result = new Input('phpunit');
+		$this->assertInstanceOf( Input::class, $result );
+	}
+
+	/**
+	 *
+	 *
+	 */
+	public function testInputIsEmptyBeforePost() {
+		$result = new Input('phpunit');
+		$this->assertFalse( $result->is_not_empty() );
+	}
+
+	/**
+	 *
+	 *
+	 */
+	public function testInputIsNotEmptyAfterPost() {
+		$_POST = self::$http_post;
+		$result = new Input('phpunit');
+		$this->assertTrue( $result->is_not_empty() );
+	}
 }
