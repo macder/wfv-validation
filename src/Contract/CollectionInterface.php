a82e7a616a06d66514a27cfe130dbdc8f53d2392
Maciej Derulski <macder7@gmail.com>
2017-04-29 01:11:40 -0400

    phpdoc block templates

--- src/Contract/CollectionInterface.php
+++ src/Contract/CollectionInterface.php
@@ -8,8 +8,24 @@ defined( 'ABSPATH' ) or die();
  * @since 0.10.0
  */
 interface CollectionInterface {
+
+	/**
+	 * @return
+	 */
 	public function has( $key );
+
+	/**
+	 * @return
+	 */
 	public function is_not_empty();
+
+	/**
+	 * @return
+	 */
 	public function render( $value, callable $callback );
+
+	/**
+	 * @return
+	 */
 	public function transform( $value, callable $callback );
 }
