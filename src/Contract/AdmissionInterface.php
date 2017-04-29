edbc6cd07f90262caa19278522dfc0b46d75d460
Maciej Derulski <macder7@gmail.com>
2017-04-28 20:00:00 -0400

    AdmissionInterface - base skeleton

---
+++ src/Contract/AdmissionInterface.php
@@ -0,0 +1,17 @@
+<?php
+namespace WFV\Contract;
+defined( 'ABSPATH' ) or die();
+
+/**
+ *
+ *
+ * @since 0.10.0
+ *
+ */
+interface AdmissionInterface {
+
+	/**
+	 * @return
+	 */
+	// public function create( $action );
+}
