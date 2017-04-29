1fdffd00035cdd4c3f7a9e3151a407c822e56e97
Maciej Derulski <macder7@gmail.com>
2017-04-28 21:34:34 -0400

    Messages - clear legacy code, make it fresh

--- src/Component/Messages.php
+++ src/Component/Messages.php
@@ -19,20 +19,4 @@ class Messages {
 	function __construct( $messages = null ) {
 
 	}
-
-	/**
-	 * Check if a fields rule has custom message
-	 *
-	 * @since 0.7.0
-	 *
-	 * @param string $field
-	 * @param string $rule
-	 * @return bool
-	 */
-	public function exist( $field, $rule ) {
-		if( property_exists( $this, $field ) && array_key_exists( $rule, $this->$field ) ) {
-			return true;
-		}
-		return false;
-	}
 }
