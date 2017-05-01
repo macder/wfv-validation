00315e886a91102817ac895e3440bb26952fbd14
Maciej Derulski <macder7@gmail.com>
2017-04-30 22:10:37 -0400

    Inspector - Base structure

--- src/Composite/Inspector.php
+++ src/Composite/Inspector.php
@@ -2,11 +2,27 @@
 namespace WFV\Composite;
 defined( 'ABSPATH' ) or die();
 
+use WFV\Abstraction\Composable;
+
 /**
  *
  *
  * @since 0.10.0
  */
-class Inspector {
+class Inspector extends Composable {
+
+	private $token;
+
+	/**
+	 *
+	 *
+	 * @since 0.10.0
+	 *
+	 * @param
+	 * @param
+	 */
+	function __construct() {
+
+	}
 
 }
