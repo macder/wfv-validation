f54c12f77692d249d5542933f1c78b89787aab78
Maciej Derulski <macder7@gmail.com>
2017-05-01 07:59:26 -0400

    Collectable - porting back collection methods and tests

--- src/Abstraction/Collectable.php
+++ src/Abstraction/Collectable.php
@@ -11,4 +11,94 @@ use WFV\Contract\CollectionInterface;
  */
 abstract class Collectable implements CollectionInterface {
 
+	/**
+	 *
+	 *
+	 * @since 0.10.0
+	 * @access protected
+	 * @var array
+	 */
+	protected $data = array();
+
+	/**
+	 *
+	 *
+	 * @since 0.8.6
+	 *
+	 * @return bool
+	 */
+	public function contains( $key = null, $value = null ) {
+		// WIP
+		if( $this->has( $key ) ) {
+			if ( is_array( $this->data[ $key ] ) ) {
+				return ( in_array( $value, $this->data[ $key ] ) ) ? true : false;
+			}
+			return ( $this->data[ $key ] === $value ) ? true : false;
+		}
+		return false;
+	}
+
+	/**
+	 *
+	 *
+	 * @since 0.10.0
+	 *
+	 * @param string $property
+	 * @return bool
+	 */
+	public function has( $key = null ) {
+		return array_key_exists( $key, $this->data );
+	}
+
+	/**
+	 *
+	 *
+	 * @since 0.10.0
+	 *
+	 * @return bool
+	 */
+	public function is_populated() {
+		return ( count( $this->data ) > 0 ) ? true : false;
+	}
+
+	/**
+	 *
+	 *
+	 * @since 0.9.0
+	 *
+	 * @param string $field
+	 * @param string|array (optional) $callback
+	 * @return string|null
+	 */
+	public function render( $key, callable $callback = null ) {
+		if( is_string( $key ) ) {
+			$callback = ( null === $callback ) ? 'esc_html' : $callback;
+			return ( true === $this->has( $key ) ) ? $this->call_func( $callback, $this->data[ $key ] ) : null;
+		}
+		return null;
+	}
+
+	/**
+	 * Trigger a callback function
+	 *
+	 * @since 0.9.0
+	 * @access private
+	 *
+	 * @param string|array $callback
+	 * @param string (optional) $input The input string
+	 * @return
+	 */
+	protected function call_func( $callback, $input = null ) {
+		// WIP - simplify
+		if( is_array( $callback ) ) {
+			$method = $callback[0];
+			$args = $callback[1];
+			if ( $input ) {
+				array_unshift( $args, $input );
+			}
+			return call_user_func_array( $method, $args );
+		}
+		return $callback( $input );
+	}
+
 }
---
+++ tests/src/Abstraction/CollectableTest.php
@@ -0,0 +1,242 @@
+<?php
+namespace WFV\Abstraction;
+
+use WFV\Abstraction\Collectable;
+
+class Collection extends Collectable {
+
+	function __construct( $data ) {
+		$this->data = $data;
+	}
+
+}
+
+class CollectableTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 *
+	 *
+	 * @access protected
+	 * @var
+	 */
+	protected static $collection;
+
+	/**
+	 *
+	 *
+	 * @access protected
+	 * @var
+	 */
+	protected static $data;
+
+	/**
+	 *
+	 *
+	 */
+  protected function setUp() {
+
+  	self::$data = array(
+			'action' => 'phpunit',
+			'name' => 'Foo Bar',
+			'email' => 'foo@bar.com',
+			'shades' => array(
+				'lightest',
+				'light',
+				'dark',
+				'darkest',
+			),
+			'color' => array(
+				'red',
+				'green',
+				'blue',
+			),
+			'html_input' => '<h1>Im a H1</h1><p>This is a paragraph</p>'
+  	);
+
+  	self::$collection = new Collection( self::$data );
+
+  	// print_r(self::$collection);
+	}
+
+	/**
+	 *
+	 *
+	 */
+	protected function tearDown() {
+
+	}
+
+	/**
+	 * has() returns true when key exists in $data property
+	 *
+	 */
+  public function test_collection_has_returns_true() {
+  	$result = self::$collection->has( 'name' );
+  	$this->assertTrue( $result );
+	}
+
+	/**
+	 * has() returns false when key is NOT in $data property
+	 *
+	 */
+  public function test_collection_has_returns_false() {
+  	$result = self::$collection->has( 'this_key_is_not_there' );
+  	$this->assertFalse( $result );
+	}
+
+	/**
+	 * has() returns false when key is null
+	 *
+	 */
+  public function test_collection_has_returns_false_when_key_null() {
+  	$result = self::$collection->has( null );
+  	$this->assertFalse( $result );
+	}
+
+	/**
+	 * contains() returns true when key value is a string that matches
+	 *
+	 */
+  public function test_collection_contains_returns_true_when_string() {
+  	$result = self::$collection->contains( 'email', 'foo@bar.com' );
+  	$this->assertTrue( $result );
+	}
+
+	/**
+	 * contains() returns true when key value is an array, and the string is in array
+	 *
+	 */
+  public function test_collection_contains_returns_true_when_array() {
+  	$result = self::$collection->contains( 'color', 'green' );
+  	$this->assertTrue( $result );
+	}
+
+	/**
+	 * contains() returns false when string NOT there
+	 *
+	 */
+  public function test_collection_contains_returns_false_when_string() {
+  	$result = self::$collection->contains( 'email', 'im_not_there' );
+  	$this->assertFalse( $result );
+	}
+
+	/**
+	 * contains() returns false when key value is an array, and string is not there
+	 *
+	 */
+  public function test_collection_contains_returns_false_when_array() {
+  	$result = self::$collection->contains( 'color', 'im_not_there' );
+  	$this->assertFalse( $result );
+	}
+
+	/**
+	 * contains() returns false when $key is empty string
+	 *
+	 */
+  public function test_collection_contains_returns_false_when_key_is_empty_string() {
+  	$result = self::$collection->contains( '', 'im_not_there' );
+  	$this->assertFalse( $result );
+	}
+
+	/**
+	 * contains() returns false when $key is not there
+	 *
+	 */
+  public function test_collection_contains_returns_false_when_key_is_not_there() {
+  	$result = self::$collection->contains( 'im_not_there', 'red' );
+  	$this->assertFalse( $result );
+	}
+
+	/**
+	 * contains() returns false when $key is null
+	 *
+	 */
+  public function test_collection_contains_returns_false_when_key_is_null() {
+  	$result = self::$collection->contains( null, 'red' );
+  	$this->assertFalse( $result );
+	}
+
+	/**
+	 * contains() returns false when $key rxists but $value null
+	 *
+	 */
+  public function test_collection_contains_returns_false_when_key_exists_but_value_null() {
+  	$result = self::$collection->contains( 'color', null );
+  	$this->assertFalse( $result );
+	}
+
+	/**
+	 * contains() returns false when $key and $value null
+	 *
+	 */
+  public function test_collection_contains_returns_false_when_key_value_nulls() {
+  	$result = self::$collection->contains( null, null );
+  	$this->assertFalse( $result );
+	}
+
+	/**
+	 * is_populated() returns true
+	 *
+	 */
+  public function test_collection_is_populated_returns_true() {
+  	$result = self::$collection->is_populated();
+  	$this->assertTrue( $result );
+	}
+
+	/**
+	 * is_populated() returns false when $data is empty array
+	 *
+	 */
+  public function test_collection_is_populated_returns_false() {
+  	$result = new Collection( $data = array() );
+  	$this->assertFalse( $result->is_populated() );
+	}
+
+	/**
+	 * render() returns escaped html
+	 *
+	 */
+  public function test_collection_render_default_returns_encoded_string() {
+  	$expected = esc_html( self::$data['html_input'] );
+  	$result = self::$collection->render('html_input');
+
+  	// $result = ( $expected === $result ) ? true : false;
+
+  	$this->assertEquals( $expected, $result );
+	}
+
+	/**
+	 * render() returns encoded string from a callable
+	 *
+	 */
+  public function test_collection_render_returns_encoded_string_from_callable() {
+  	$expected = strip_tags( self::$data['html_input'] );
+  	$result = self::$collection->render('html_input', 'strip_tags');
+
+  	$this->assertEquals( $expected, $result );
+	}
+
+	/**
+	 * render() returns result from closure
+	 *
+	 */
+  public function test_collection_render_returns_string_from_closure() {
+  	$expected = 'phpunit';
+  	$result = self::$collection->render('name', function( $input ) {
+  		return 'phpunit';
+  	});
+
+  	$this->assertEquals( $expected, $result );
+	}
+
+	/**
+	 * render() returns null when key is not found
+	 *
+	 */
+  public function test_collection_render_returns_null_when_key_not_there() {
+  	$expected = null;
+  	$result = self::$collection->render('im_not_there');
+
+  	$this->assertEquals( $expected, $result );
+	}
+}
