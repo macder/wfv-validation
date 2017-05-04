eead673e1b7bdc7c89b44ddb9f07b645c9f4f009
Maciej Derulski <macder7@gmail.com>
2017-05-03 23:46:36 -0400

    Form - porting in convienience methods

--- src/Composite/Form.php
+++ src/Composite/Form.php
@@ -4,6 +4,7 @@ defined( 'ABSPATH' ) or die();
 
 use WFV\Abstraction\Composable;
 use WFV\Contract\ValidationInterface;
+
 /**
  *
  *
@@ -30,34 +31,73 @@ class Form extends Composable {
 	 *
 	 * @since 0.10.0
 	 *
+	 * @param string $rule
 	 * @param string $field
-	 * @return WFV\Component\Errors
 	 */
-	public function errors( $field = null ) {
+	public function add_rule( $rule, $field ) {
+		$this->adapter('validator')->add_rule( $rule, $field );
+	}
+
+	/**
+	 * Convenience method to repopulate checkbox input
+	 *
+	 * @since 0.10.0
+	 *
+	 * @param string $field Field name.
+	 * @param string $value Value to compare against.
+	 * @return string|null
+	 */
+	public function checked_if( $field = null, $value = null ) {
+		return $this->string_or_null( 'checked', $field, $value );
+	}
+
+	/**
+	 * Get input instance
+	 *
+	 * @since 0.10.0
+	 *
+	 * @return WFV\Component\ErrorCollection
+	 */
+	public function errors() {
 		return $this->utilize('errors');
 	}
 
 	/**
-	 *
+	 * Get input instance
 	 *
 	 * @since 0.10.0
 	 *
-	 * @param string $field
-	 * @return WFV\Component\Input
+	 * @return WFV\Component\InputCollection
 	 */
-	public function input( $field = null ) {
+	public function input() {
 		return $this->utilize('input');
+	}
+
+	/**
+	 * Convenience method to repopulate select input
+	 *
+	 * @since 0.10.0
+	 *
+	 * @param string $field Field name.
+	 * @param string $value Value to compare against.
+	 * @return string|null
+	 */
+	public function selected_if( $field = null, $value = null ) {
+		return $this->string_or_null( 'selected', $field, $value );
 	}
 
 	/**
 	 *
 	 *
 	 * @since 0.10.0
+	 * @access private
 	 *
-	 * @param string $rule
-	 * @return
+	 * @param string $response
+	 * @param string (optional) $field
+	 * @param string (optional) $value
+	 * @return string|null
 	 */
-	public function add_rule( $rule, $field ) {
-		$this->adapter('validator')->add_rule( $rule, $field );
+	private function string_or_null( $response, $field = null, $value = null ) {
+		return ( $this->input( $field )->contains( $field, $value ) ) ? $response : null;
 	}
 }
