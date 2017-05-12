51eed2ea38dd84cc3590679be5cfbc5b88071ffd
Maciej Derulski <macder7@gmail.com>
2017-05-12 01:38:53 -0400

    WIP - RulesCollection - reworking to simplified client rule definition

--- src/Collection/RuleCollection.php
+++ src/Collection/RuleCollection.php
@@ -18,9 +18,10 @@ class RuleCollection extends Collectable {
 	 *
 	 * @param array $rules
 	 */
-	public function __construct( array $rules = [] ) {
-		$this->data = $rules;
+	public function __construct( array $rules ) {
+		$this->data = $this->parse_rules( $rules );
 	}
+
 
 	/**
 	 * Get rules array
@@ -34,6 +35,87 @@ class RuleCollection extends Collectable {
 	}
 
 	/**
+	 *
+	 *
+	 * @since 0.11.0
+	 * @access private
+	 *
+	 * @param string $rule
+	 * @return bool
+	 */
+	private function extract_name( $rule ) {
+		return strstr( $rule, ':', true );
+	}
+
+	/**
+	 *
+	 *
+	 * @since 0.11.0
+	 * @access private
+	 *
+	 * @param string $rule
+	 * @return bool
+	 */
+	private function extract_params( $rule ) {
+		return ltrim( strstr($rule, ':'), ':');
+	}
+
+	/**
+	 * Checks if a rule string has parameters
+	 *
+	 * @since 0.11.0
+	 * @access private
+	 *
+	 * @param string $rule
+	 * @return bool
+	 */
+	private function has_parameters( $rule ) {
+		return strpos( $rule, ':' );
+	}
+
+	/**
+	 *
+	 *
+	 * @since 0.11.0
+	 * @access private
+	 *
+	 * @param array $rules
+	 * @return array
+	 */
+	private function parse_rules( array $rules ) {
+
+		// WIP - not working properly... simplify!
+		$rules = $this->split_rules( $rules );
+		return array_map( function( $item ) {
+			foreach( $item as $rule ) {
+				if ( $this->has_parameters( $rule ) ) {
+					$array_rule = array([
+						'rule' => $this->extract_name( $rule ),
+						'params' => explode( ',', $this->extract_params( $rule ) )
+					]);
+				}
+			}
+			return ( $array_rule ) ? $array_rule : $item;
+		}, $rules );
+	}
+
+	/**
+	 * Converts string ruleset to index array
+	 *
+	 * @since 0.11.0
+	 * @access private
+	 *
+	 * @param array $rules
+	 * @return array
+	 */
+	private function split_rules( array $rules ) {
+		// perhaps the $rules array structure should be validated here?...
+		return array_map( function( $item ) {
+			return explode( '|', $item );
+		}, $rules );
+	}
+
+	/**
 	 * Check if rule is custom
 	 *
 	 * @since 0.10.0
@@ -42,6 +124,7 @@ class RuleCollection extends Collectable {
 	 * @return bool
 	 */
 	public function is_custom( $rule ) {
+		// WIP - needs adjusting to new rule string format
 		if ( false === is_array( $rule ) ){
 			return ( false !== strpos( $rule, 'custom:' ) ) ? true : false;
 		}
