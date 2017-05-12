87e362758af534953b7550481e86db12ebe8977a
Maciej Derulski <macder7@gmail.com>
2017-05-12 12:35:49 -0400

    wfv - remove not needed 'use'

--- wfv-validate.php
+++ wfv-validate.php
@@ -4,27 +4,23 @@ defined( 'ABSPATH' ) or die();
 Plugin Name: WFV - Form Validation
 Plugin URI:  https://macder.github.io/wfv/
 Description: A simple fluid and concise API to manage user input, validation, feedback, and safe output.
-Version:     0.10.1
+Version:     0.11.0
 Author:      Maciej Derulski
 Author URI:  https://github.com/macder
 License:     BSD 3-Clause
 License URI: https://github.com/macder/wp-form-validation/blob/master/LICENSE
 */
 
-define( 'WFV_VALIDATE_VERSION', '0.10.1' );
+define( 'WFV_VALIDATE_VERSION', '0.11.0' );
 define( 'WFV_VALIDATE__MINIMUM_WP_VERSION', '3.7' );
 define( 'WFV_VALIDATE__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'WFV_VALIDATE__ACTION_POST', 'validate_form' );
 
 require_once WFV_VALIDATE__PLUGIN_DIR . '/vendor/autoload.php';
 
-use \Respect\Validation\Validator;
 use WFV\Agent\InspectionAgent;
 use WFV\Artisan\Director;
 use WFV\Artisan\FormArtisan;
-
-use WFV\Validators;
-
 
 /**
  *
