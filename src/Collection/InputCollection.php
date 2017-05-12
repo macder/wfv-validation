c00980ecfa981bc66f590a8f7463655bb759ff82
Maciej Derulski <macder7@gmail.com>
2017-05-12 12:18:01 -0400

    InputCollection - new method to retrieve the array
    need it to pass values during validation

--- src/Collection/InputCollection.php
+++ src/Collection/InputCollection.php
@@ -22,4 +22,15 @@ class InputCollection extends Collectable {
 	function __construct( array $data = array() ) {
 		$this->data = $this->transform_array_leafs( $data, 'stripslashes' );
 	}
+
+	/**
+	 *
+	 *
+	 * @since 0.11.0
+	 *
+	 * @return array
+	 */
+	public function get_array() {
+		return $this->data;
+	}
 }
