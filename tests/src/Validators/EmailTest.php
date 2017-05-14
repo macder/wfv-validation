4921379e232d44002648ee443880a6a4587cb36a
Maciej Derulski <macder7@gmail.com>
2017-05-14 00:47:27 -0400

    tests - validator - email - complete

--- tests/src/Validators/EmailTest.php
+++ tests/src/Validators/EmailTest.php
@@ -7,27 +7,61 @@ use \Respect\Validation\Validator;
 class EmailTest extends \PHPUnit_Framework_TestCase {
 
 	/**
-	 * Does email validation return true when input is email format string?
+	 * Does email validation return true when
+	 *  validation is NOT optional and
+	 *  input is email format string?
 	 *
 	 */
 	public function test_email_returns_true_when_string_is_email_format() {
-		$email = new Email( new Validator(), 'test_field' );
+		$optional = false;
+		$email = new Email( new Validator(), 'test_field', $optional );
 		$result = $email->validate('foo@bar.com');
 		$this->assertTrue( $result );
 	}
 
 	/**
-	 * Does email validation return false when input is NOT email format string?
+	 * Does email validation return false when
+	 *  validation is NOT optional and
+	 *  input is NOT email format string?
 	 *
 	 */
 	public function test_email_returns_false_when_string_not_email_format() {
-		$email = new Email( new Validator(), 'test_field' );
+		$optional = false;
+		$email = new Email( new Validator(), 'test_field', $optional );
 		$result = $email->validate('foobarcom');
 		$this->assertFalse( $result );
 	}
 
 	/**
-	 * Does email validation return true when optional and is empty?
+	 * Does email validation return false when
+	 *  validation is NOT optional and
+	 *  input is empty?
+	 *
+	 */
+	public function test_email_returns_false_when_not_optional_and_empty() {
+		$optional = false;
+		$email = new Email( new Validator(), 'test_field', $optional );
+		$result = $email->validate('');
+		$this->assertFalse( $result );
+	}
+
+	/**
+	 * Does email validation return true when
+	 *  validation is optional and
+	 *  input is valid email string?
+	 *
+	 */
+	public function test_email_returns_true_when_optional_and_valid_email() {
+		$optional = true;
+		$email = new Email( new Validator(), 'test_field', $optional );
+		$result = $email->validate('foo@bar.com');
+		$this->assertTrue( $result );
+	}
+
+	/**
+	 * Does email validation return true when
+	 *  validation is optional and
+	 *  input is empty?
 	 *
 	 */
 	public function test_email_returns_true_when_optional_and_empty() {
@@ -38,13 +72,15 @@ class EmailTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	/**
-	 * Does email validation return false when NOT optional and is empty?
+	 * Does email validation return false when
+	 *  validation is optional and
+	 *  input is not valid email string?
 	 *
 	 */
-	public function test_email_returns_false_when_not_optional_and_empty() {
-		$optional = false;
+	public function test_email_returns_false_when_optional_and_invalid_email_string() {
+		$optional = true;
 		$email = new Email( new Validator(), 'test_field', $optional );
-		$result = $email->validate('');
+		$result = $email->validate('foobarcom');
 		$this->assertFalse( $result );
 	}
 }
