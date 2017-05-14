4b7bca1002e479b604846850954f249e6b552384
Maciej Derulski <macder7@gmail.com>
2017-05-13 23:46:20 -0400

    tests - required (if / with) - pass optioanl param as false
    after adding support for 'optional' validation, we must pass a bool param

--- tests/src/Validators/RequiredIfTest.php
+++ tests/src/Validators/RequiredIfTest.php
@@ -22,7 +22,7 @@ class RequiredIfTest extends \PHPUnit_Framework_TestCase {
 			'other_field',
 			'is this value'
 		);
-		self::$required_if = new RequiredIf( new Validator(), 'test_field', $params );
+		self::$required_if = new RequiredIf( new Validator(), 'test_field', false, $params );
 	}
 
 	/**
