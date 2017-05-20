ebeaa5f7e5bd624921a24e7f6541d3a3fb1b6ffa
Maciej Derulski <macder7@gmail.com>
2017-05-19 23:19:15 -0400

    closes #97 - new validator and tests for 'digit'

---
+++ src/Validators/Digit.php
@@ -0,0 +1,41 @@
+<?php
+namespace WFV\Validators;
+defined( 'ABSPATH' ) or die();
+
+use WFV\Validators\AbstractValidator;
+
+/**
+ *
+ *
+ * @since 0.11.0
+ */
+class Digit extends AbstractValidator {
+
+	/**
+	 *
+	 *
+	 * @since 0.11.0
+	 * @access protected
+	 * @var array
+	 */
+	protected $template = [
+		'message' => '{label} must be a digit',
+		'name' => 'digit',
+	];
+
+	/**
+	 * Set the validation constraints that make this rule
+	 *
+	 * @since 0.11.0
+	 *
+	 * @param bool (optional) $optional
+	 * @return self
+	 */
+	public function set_policy( $optional = false ) {
+		$v = $this->validator;
+		$v = ( $optional )
+			? $v->optional( $v->create()->digit()->length(1,1) )
+			: $v->digit()->length(1,1);
+		return $this;
+	}
+}
