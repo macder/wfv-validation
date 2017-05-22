4e358fb7a86b4d03ed2c5f4029ca962ad4f93f53
Maciej Derulski <macder7@gmail.com>
2017-05-22 04:15:54 -0400

    closes #133 - new validator and tests for 'phone' rule

---
+++ tests/src/Validators/PhoneTest.php
@@ -0,0 +1,83 @@
+<?php
+namespace WFV\Validators;
+
+use \Respect\Validation\Validator;
+
+
+class PhoneTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 *
+	 *
+	 * @access protected
+	 * @var ValidateInterface
+	 */
+	protected static $validator;
+
+	/**
+	 *
+	 *
+	 */
+	protected function setUp() {
+		self::$validator = new Phone();
+	}
+
+	/**
+	 * Reset
+	 *
+	 */
+	protected function tearDown() {
+		self::$validator = null;
+	}
+
+	/**
+	 * Does phone validation return true when
+	 *  validation is NOT optional and
+	 *  input is 10 digit phone number
+	 *  with brackets around area code?
+	 *
+	 */
+	public function test_phone_returns_true_when_not_optional_input_phone_10_area_bracket() {
+		$optional = false;
+		$result = self::$validator->validate('(555)555-5555', $optional);
+		$this->assertTrue( $result );
+	}
+
+	/**
+	 * Does phone validation return true when
+	 *  validation is optional and
+	 *  input is 10 digit phone number
+	 *  with brackets around area code?
+	 *
+	 */
+	public function test_phone_returns_true_when_optional_input_phone_10_area_bracket() {
+		$optional = true;
+		$result = self::$validator->validate('(555)555-5555', $optional);
+		$this->assertTrue( $result );
+	}
+
+	/**
+	 * Does phone validation return false when
+	 *  validation is NOT optional and
+	 *  input not phone number?
+	 *
+	 */
+	public function test_phone_returns_false_when_not_optional_input_not_phone() {
+		$optional = false;
+		$result = self::$validator->validate('not a phone num', $optional);
+		$this->assertFalse( $result );
+	}
+
+	/**
+	 * Does phone validation return false when
+	 *  validation is optional and
+	 *  input not phone number?
+	 *
+	 */
+	public function test_phone_returns_false_when_optional_input_not_phone() {
+		$optional = true;
+		$result = self::$validator->validate('not a phone num', $optional);
+		$this->assertFalse( $result );
+	}
+
+}
